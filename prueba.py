76c3a1dc9f69b2a6d6754cf1f8430433b4c7eb98
cvitola <isrunaway@gmail.com>
2024-12-28 10:09:17 -0300

    agrega variables para probar

--- prueba.py
+++ prueba.py
@@ -10,12 +10,19 @@ from_whatsapp_number = os.getenv("FROM_WHATSAPP_NUMBER")
 to_whatsapp_number = os.getenv("TO_WHATSAPP_NUMBER")
 client = Client(account_sid, auth_token)
 
-def enviar_mensaje(mensaje):
+def enviar_mensaje(mensaje,telefono):
     message = client.messages.create(
         body=mensaje,
         from_=from_whatsapp_number,
-        to=to_whatsapp_number
+        to=normalizar_telefono(telefono)
     )
     print(f"Mensaje enviado: {message.sid}")
 
-enviar_mensaje("¡Hola! Este es mi primer recordatorio enviado desde Python.")
+def normalizar_telefono(telefono):
+    return f"whatsapp:+549{telefono}"
+
+nombre = input("Ingrese nombre de la persona: ")
+telefono = input("Ingrese numero de telefono de la persona: ")
+body = input("Ingrese contenido del mensaje: ")
+plantilla = f"Hola {nombre} \n{body}\nSaludos Cordiales.-"
+enviar_mensaje(plantilla,telefono)
